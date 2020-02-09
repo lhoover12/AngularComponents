f493ec2c835af1530200f1d76f82056bace3dad3
Lukas <lukashoover@hotmail.com>
2020-02-09 16:39:01 -0600

    files

---
+++ projects/my-lib/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of my-lib
+ */
+
+export * from "./lib/my-lib.service";
+export * from "./lib/my-lib.component";
+export * from "./lib/my-lib.module";
+
+export * from "./button/button.component";
+export * from "./button/button.module";
